aeae8686bbe75ad68f21192fc6a670cf49de7c7c
Anthony Pinto <anthonyp@interworldhighway.com>
2018-12-02 22:00:07 +0000

    added authentication check to controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  devise_for :users, controllers: { registrations: 'registrations' }
+  authenticated :user do
+    root to: 'users#index', as: :authenticated_root
+  end
+  devise_for :users, controllers: { registrations: 'registrations' }, :path_prefix => "r"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "users#index"
 end
