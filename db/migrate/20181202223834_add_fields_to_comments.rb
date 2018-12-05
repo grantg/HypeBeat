556bcaceee34a3e5121f9f59adb74b287433b706
Anthony Pinto <anthonyp@interworldhighway.com>
2018-12-05 18:58:49 +0000

    removed comments

--- db/migrate/20181202223834_add_fields_to_comments.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToComments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :comments, :created_by, :string
-    add_column :comments, :sent_to, :integer
-    add_reference :comments, :users, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_223834) do
-
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "created_by"
-    t.integer "sent_to"
-    t.bigint "users_id"
-    t.index ["users_id"], name: "index_comments_on_users_id"
-  end
+ActiveRecord::Schema.define(version: 2018_12_05_184938) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -38,5 +29,4 @@ ActiveRecord::Schema.define(version: 2018_12_02_223834) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "users", column: "users_id"
 end
