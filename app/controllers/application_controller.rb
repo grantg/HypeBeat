1c1d40169c13040f9cadc69c58f37965344b977f
Anthony Pinto <anthonyp@interworldhighway.com>
2018-12-05 20:16:19 +0000

    Added new dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, :get_data
+    
+    
+    def get_data
+        if user_signed_in?
+            @getFName = current_user.fname
+            @getLName = current_user.lname
+            @getEmail = current_user.email
+            @getStreet = current_user.street
+            @getState = current_user.state
+            @getCity = current_user.city
+            @getZip = current_user.zip_code
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   authenticated :user do
-    root to: 'users#index', as: :authenticated_root
+    root to: 'static_pages#dashboard', as: :authenticated_root
   end
   
   devise_for :users, controllers: { registrations: 'registrations' }, :path_prefix => "r"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root to: "users#index"
+  root to: "static_pages#dashboard"
   
   post '/user/update/:id' => 'users#update', :as => :update_user
   post '/users/new' => 'users#create', :as => :create_user
